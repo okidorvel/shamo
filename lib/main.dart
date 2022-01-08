55d608ec55cdb733ca27aa38d8ec27fbc583672f
okidorvel <okidorvel404@gmail.com>
2022-01-08 14:28:01 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/splash_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/'       : (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage()
+      },
+    );
+  }
+}
