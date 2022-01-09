28dcaff4ead1e6eec56d32a159f8d54754bbd69d
okidorvel <okidorvel404@gmail.com>
2022-01-09 20:08:15 +0700

    add chat page

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -2,11 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:shamo/theme.dart';
 
 class ChatPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('CHATPAGE', style: primaryTextStyle,),
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        centerTitle: true,
+        title: Text(
+          'Message Support',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+        elevation: 0,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          width: double.infinity,
+          color: backgroundColor3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icon_no_message.png',
+                width: 80,
+                height: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opss no message yet?',
+                style: primaryTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'You have never done a transaction',
+                style: secondaryTextStyle,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 24,
+                      ),
+                      backgroundColor: primaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      )),
+                  child: Text(
+                    'Explore Store',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 16,
+                      fontWeight: medium,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
