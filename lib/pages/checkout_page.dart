27f0037c9d4bb14f27daf7ace463e2ebd60d674a
okidorvel <okidorvel404@gmail.com>
2022-01-13 15:43:08 +0700

    add checkout page

---
+++ lib/pages/checkout_page.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/checkout_card.dart';
+
+class CheckoutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget header() {
+      return AppBar(
+        backgroundColor: backgroundColor1,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          'Checkout Details',
+          style: primaryTextStyle.copyWith(
+            fontSize: 18,
+            fontWeight: medium,
+          ),
+        ),
+      );
+    }
+
+    Widget content() {
+      return ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+        ),
+        children: [
+          // NOTE: LIST ITEMS
+          Container(
+            margin: EdgeInsetsDirectional.only(
+              top: defaultMargin,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'List Items',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                CheckoutCard(),
+                CheckoutCard(),
+              ],
+            ),
+          ),
+          // NOTE: ADDRESS DETAILS
+          Container(
+            margin: EdgeInsets.only(
+              top: defaultMargin,
+            ),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: backgroundColor4,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Address Details',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Image.asset(
+                          'assets/icon_store_location.png',
+                          width: 40,
+                        ),
+                        Image.asset(
+                          'assets/icon_line.png',
+                          height: 30,
+                        ),
+                        Image.asset(
+                          'assets/icon_your_address.png',
+                          width: 40,
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Store Location',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: light,
+                          ),
+                        ),
+                        Text(
+                          'Adidas Core',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          'Your Address',
+                          style: secondaryTextStyle.copyWith(
+                            fontSize: 12,
+                            fontWeight: light,
+                          ),
+                        ),
+                        Text(
+                          'Mars',
+                          style: primaryTextStyle.copyWith(
+                            fontWeight: medium,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor3,
+      appBar: header(),
+      body: content(),
+    );
+  }
+}
---
+++ lib/widgets/cart_card.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class CartCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: defaultMargin,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: backgroundColor4,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/image_shoes.png',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Terrex Urban Low',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text(
+                      '\$143,98',
+                      style: priceTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/button_add.png',
+                    width: 16,
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Text(
+                    '2',
+                    style: primaryTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2,
+                  ),
+                  Image.asset(
+                    'assets/button_min.png',
+                    width: 16,
+                  ),
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Image.asset(
+                'assets/icon_delete.png',
+                width: 10,
+                height: 12,
+              ),
+              SizedBox(
+                width: 4,
+              ),
+              Text(
+                'Remove',
+                style: primaryTextStyle.copyWith(
+                  color: alertColor,
+                  fontSize: 12,
+                  fontWeight: light,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
